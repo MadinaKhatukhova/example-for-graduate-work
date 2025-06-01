0d6ad9d28fd8128d34efa32a5d71825c6c1697b6
Artem <artem.yaremenko.1992@gmail.com>
2025-06-01 22:46:22 +0300

    Добавил DTO для объявлений

---
+++ src/main/java/ru/skypro/homework/dto/Ads.java
@@ -0,0 +1,19 @@
+package ru.skypro.homework.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Ads {
+
+    private Integer count;
+
+    private List<Ad> result;
+}
