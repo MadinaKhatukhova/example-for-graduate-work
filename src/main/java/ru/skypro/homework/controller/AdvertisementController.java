d0e6d60a9e5bd2aff097a2d0f988412f8d4bb84f
FunkyDm <mrforxbox@mail.ru>
2025-06-01 23:02:30 +0300

    Создан контроллер для объявлений

--- src/main/java/ru/skypro/homework/controller/AdvertisementController.java
+++ src/main/java/ru/skypro/homework/controller/AdvertisementController.java
@@ -1,43 +1,48 @@
 package ru.skypro.homework.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.Ad;
+import ru.skypro.homework.dto.Ads;
 
 @RestController
+@RequestMapping("/ads")
 public class AdvertisementController {
     @GetMapping
-    public ResponseEntity<?> getAllAds(){
-
+    public ResponseEntity<?> getAllAds() {
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping
-    public ResponseEntity<?> postAdd(){
-
+    public ResponseEntity<?> postAdd(@RequestBody Ad ad) {
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping
-    public ResponseEntity<?> getAddInfo(){
-
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getAddInfo(@RequestParam int id) {
+        return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping
-    public ResponseEntity<?> deleteAdd(){
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteAdd(@RequestParam int id) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @PatchMapping
-    public ResponseEntity<?> updateAddInfo(){
-
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> updateAddInfo(@RequestParam int id) {
+        return ResponseEntity.ok().build();
     }
 
-    @GetMapping
-    public ResponseEntity<?> getAuthUserAdds(){
-
+    @GetMapping("/me")
+    public ResponseEntity<?> getAuthUserAdds(@RequestBody Ads ads) {
+        return ResponseEntity.ok().build();
     }
 
-    @PatchMapping
-    public ResponseEntity<?> updateAddPicture(){
-
+    @PatchMapping("/{id}/image")
+    public ResponseEntity<?> updateAddPicture() {
+        return ResponseEntity.ok().build();
     }
 
 }
