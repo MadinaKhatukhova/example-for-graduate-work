7d83cf4f5c9eed588b03ffa0db2f7240c2e0bfb5
Artem <artem.yaremenko.1992@gmail.com>
2025-06-10 23:29:35 +0300

    Добавил сущность ImageEntity

---
+++ src/main/java/ru/skypro/homework/model/ImageEntity.java
@@ -0,0 +1,21 @@
+package ru.skypro.homework.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class ImageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String filePath;
+    private long fileSize;
+    private String mediaType;
+    @Lob
+    private byte[] data;
+}
