807d7225b056d0ee9aca6e7f49add14f79b3b5fe
FunkyDm <mrforxbox@mail.ru>
2025-06-18 17:39:21 +0300

    Исправления для корректной аутентификации пользователя

--- src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
@@ -1,32 +1,59 @@
 package ru.skypro.homework.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 import ru.skypro.homework.dto.Register;
+import ru.skypro.homework.mapper.UserMapper;
+import ru.skypro.homework.model.UserEntity;
 import ru.skypro.homework.service.AuthService;
 
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
-    private final UserDetailsManager manager;
+    private static final Logger log = LoggerFactory.getLogger(AuthServiceImpl.class);
+
+    @Autowired
+    private UserDetailsManager manager;
+
+    @Autowired
     private final PasswordEncoder encoder;
 
-    public AuthServiceImpl(UserDetailsManager manager,
-                           PasswordEncoder passwordEncoder) {
-        this.manager = manager;
-        this.encoder = passwordEncoder;
-    }
+    @Autowired
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Override
     public boolean login(String userName, String password) {
-        if (!manager.userExists(userName)) {
+//        if (!manager.userExists(userName)) {
+//            return false;
+//        }
+//        UserDetails userDetails = manager.loadUserByUsername(userName);
+//        return encoder.matches(password, userDetails.getPassword());
+
+        log.info("username={}, password={}", userName, password);
+        try{
+            Authentication authenticationToken = new UsernamePasswordAuthenticationToken(userName, password);
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            return true;
+        } catch (Exception e){
+            log.error("Не удалось войти в систему для пользователя: {}", userName, e);
             return false;
         }
-        UserDetails userDetails = manager.loadUserByUsername(userName);
-        return encoder.matches(password, userDetails.getPassword());
     }
 
     @Override
@@ -34,13 +61,15 @@ public class AuthServiceImpl implements AuthService {
         if (manager.userExists(register.getUsername())) {
             return false;
         }
-        manager.createUser(
-                User.builder()
-                        .passwordEncoder(this.encoder::encode)
-                        .password(register.getPassword())
-                        .username(register.getUsername())
-                        .roles(register.getRole().name())
-                        .build());
+//        manager.createUser(
+//                User.builder()
+//                        .passwordEncoder(this.encoder::encode)
+//                        .password(register.getPassword())
+//                        .username(register.getUsername())
+//                        .roles(register.getRole().name())
+//                        .build());
+        UserEntity userEntity = userMapper.registerToUserEntity(register);
+        userEntity.setPassword(encoder.encode(register.getPassword()));
         return true;
     }
 
