e32dd7d2a645658efb2dd77fd6d245975db26344
M_8849 <Madina-Khat@yandex.ru>
2025-06-19 22:27:54 +0300

    Добавление методов в UserController

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -1,17 +1,19 @@
 package ru.skypro.homework.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.NewPasswordDTO;
 import ru.skypro.homework.dto.UpdateUserDTO;
 import ru.skypro.homework.dto.UserDTO;
 import ru.skypro.homework.exception.NotEditUserPasswordException;
 import ru.skypro.homework.service.ImageService;
 import ru.skypro.homework.service.UserService;
+
+import java.io.IOException;
 
 @Slf4j
 @CrossOrigin(value = "http://localhost:3000")
@@ -52,6 +54,23 @@ public class UserController {
                                                     Authentication authentication) {
         String username = authentication.getName();
         return ResponseEntity.ok(userService.updateUser(Long.valueOf(username), updateUser));
+    }
+    @PatchMapping("/me/image")
+//    @Operation(summary = "Обновление аватара авторизованного пользователя")
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "200", description = "OK"),
+//            @ApiResponse(responseCode = "401", description = "Unauthorized")
+//    })
+    public ResponseEntity<?> updateUserImage(@RequestParam("image") MultipartFile image,
+                                             Authentication authentication) throws IOException {
+        try {
+            String username = authentication.getName();
+            imageService.uploadImage(Long.valueOf(username), image);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            log.warn("NotSaveAvatarException in save image");
+            return ResponseEntity.badRequest().build();
+        }
     }
 
 }
